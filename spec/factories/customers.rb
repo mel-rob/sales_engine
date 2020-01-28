cf3a0ede0428b91dd5e942df198e4c7308f33f6e
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-28 16:07:30 -0700

    Add FactoryBot definition for Customer

---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { "I'm a first name!" }
+    last_name { "I'm a last name!" }
+    created_at { "2012-03-27 14:53:59 UTC" }
+    updated_at { "2012-03-27 14:53:59 UTC" }
+  end
+end

2b0d26a3568873b09cae3d7dfe92c7dbd9603ac9
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-28 15:46:22 -0700

    Add ItemsController

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::ItemsController < ApplicationController
+
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    render json: ItemSerializer.new(merchant.items)
+  end
+
+end

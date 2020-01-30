0f24191210cf51d49629d8d8e7ef8bc84be8f6d3
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-30 16:06:02 -0700

    Add Invoice Items Item Controller

---
+++ app/controllers/api/v1/invoice_items/item_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::InvoiceItems::ItemController < ApplicationController
+
+  def show
+    invoice_item = InvoiceItem.find(params[:id])
+    render json: ItemSerializer.new(invoice_item.item)
+  end
+end

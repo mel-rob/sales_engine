031704db487d3e51519a60a81ada35d265800df5
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-30 12:10:27 -0700

    Add Invoices Customer Controller

---
+++ app/controllers/api/v1/invoices/customer_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Invoices::CustomerController < ApplicationController
+
+  def show
+    invoice = Invoice.find(params[:id])
+    render json: CustomerSerializer.new(invoice.customer)
+  end
+end

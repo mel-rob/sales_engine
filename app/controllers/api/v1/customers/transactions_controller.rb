28583298bf81b41da09cf93a3df08436ca5db5f9
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-29 22:21:46 -0700

    Add Customers Transactions Controller

---
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Customers::TransactionsController < ApplicationController
+
+  def index
+    customer = Customer.find(params[:id])
+    render json: TransactionSerializer.new(customer.transactions)
+  end
+end

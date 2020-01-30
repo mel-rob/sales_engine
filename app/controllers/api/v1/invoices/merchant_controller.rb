148159389ada5497fe5dd5d265c13db5db7ea5ee
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-30 12:10:53 -0700

    Add Invoices Transactions Controller

---
+++ app/controllers/api/v1/invoices/merchant_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Invoices::MerchantController < ApplicationController
+
+  def show
+    invoice = Invoice.find(params[:id])
+    render json: MerchantSerializer.new(invoice.merchant)
+  end
+end

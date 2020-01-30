384d5e2005903292c4f7c7fb126552c6161b462c
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-30 15:44:54 -0700

    Include Dollarable module in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
+require_relative 'modules/dollarable'
+
 class Item < ApplicationRecord
+  include Dollarable
+
+  before_validation :change_to_dollars
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items

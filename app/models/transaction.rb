d79e1dfb2c34c3008038e45991fbae5eebe08661
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-02 11:35:56 -0700

    Add relationship to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,7 @@ class Transaction < ApplicationRecord
   has_many :invoice_items, through: :invoice
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :customers, through: :items
 
   scope :successful, -> { where(result: "success") }
 end
